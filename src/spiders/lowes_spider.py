3b761c433e1bab10d1bdf36ca59419a67db41347
perebaj <perebaj@gmail.com>
2020-10-23 22:40:40 -0300

    review code

--- src/spiders/lowes_spider.py
+++ src/spiders/lowes_spider.py
@@ -24,6 +24,7 @@ class LowesSpider(scrapy.Spider):
     products_id_list = []
 
     def start_requests(self):
+
         urls = ['https://www.lowes.com/c/Refrigerators-Appliances']
         for url in urls:
             yield scrapy.Request(url=url, callback=self.href_parse)
@@ -37,21 +38,24 @@ class LowesSpider(scrapy.Spider):
     def page_parse(self, response):
         data = json.loads(re.findall(
             '__PRELOADED_STATE__ = ([^<]+)</script>', str(response.text))[0])
-        itemsPage = int(data['itemCount'])
-        numPages = 1 if int(itemsPage/36) == 0 else int(itemsPage/36)+1
-        for page in range(numPages):
-            newPage = response.url + f"?offset={(36*page)}"
+        total_items = int(data['itemCount'])
+        items_per_page = 36
+        num_pages = 1 if int(total_items/items_per_page) == 0 else int(total_items/items_per_page)+1
+        for page in range(num_pages):
+            newPage = response.url + f"?offset={(items_per_page*page)}"
             yield scrapy.Request(url=newPage, callback=self.id_parse, dont_filter=True)
 
     def id_parse(self, response):
         data = json.loads(re.findall(
             '__PRELOADED_STATE__ = ([^<]+)</script>', str(response.text))[0])
-
         for product in data['itemList']:
             product_id = product['product']['omniItemId']
             product_detail_href = f'/pd/{product_id}/productdetail/2707/Guest'
             product_review_href = f'/rnr/r/get-by-product/{product_id}/pdp/prod'
             product_detail_url = self.URL + product_detail_href
             product_review_url = self.URL + product_review_href
+            print(f"- Send url {product_review_url}...")
+            # print(f"- send message {product_review_url}...")
+
             # send_message(product_detail_url, self.product_queue_url)
             send_message(product_review_url, self.review_queue_url)
